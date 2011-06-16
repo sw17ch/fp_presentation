0e3e6434492e1e50017d3dd4d1e7eda3f3233b8b
John Van Enk <vanenkj@gmail.com>
2011-06-16 13:52:01 -0400

    Added list example.

--- snippets/hs/lists.hs
+++ snippets/hs/lists.hs
@@ -5,6 +5,7 @@ x = [1, 2, 3]
 y = [1..3]
 allPositive = [1..] -- infinite!
 allPositiveEvens = [x | x `div` 2 == 0] -- expressive!
+fibonacci = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 -- Matching a list
 head (x:xs) = x
